fad23dbca5155b66f099f3e22cc7f377794eb099
tatsuro <oscarwild13@gmail.com>
2021-07-11 19:13:34 +0900

    apolloとNext.js連携

---
+++ pages/hasura-main.tsx
@@ -0,0 +1,35 @@
+import { useQuery } from '@apollo/client';
+import Link from 'next/link';
+import React from 'react';
+import { VFC } from 'react';
+import Layout from '../components/Layout';
+import { GET_USERS } from '../queries/queries';
+import { GetUsersQuery } from '../types/generated/graphql';
+
+const FetchMain: VFC = () => {
+  const { data, error } = useQuery<GetUsersQuery>(GET_USERS);
+  if (error) {
+    return (
+      <Layout title="Hasura fetchPolicy">
+        <p>Error: {error.message}</p>
+      </Layout>
+    );
+  }
+  return (
+    <Layout title="Hasura fetchPolicy">
+      <p className="mb-6 font-bold">Hasura main Page</p>
+      {data?.users.map((user) => {
+        return (
+          <p className="my-1" key={user.id}>
+            {user.name}
+          </p>
+        );
+      })}
+      <Link href="/hasura-sub">
+        <a className="mt-6">Next</a>
+      </Link>
+    </Layout>
+  );
+};
+
+export default FetchMain;
