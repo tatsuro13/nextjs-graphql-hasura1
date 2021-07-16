ef838d5dc85766d7097581e389c60fcc8d5f8db7
tatsuro <oscarwild13@gmail.com>
2021-07-16 10:49:40 +0900

    エンドポイント環境変数に変更

--- lib/apploClient.ts
+++ lib/apploClient.ts
@@ -6,14 +6,16 @@ import {
 } from '@apollo/client';
 import 'cross-fetch/polyfill';
 
-export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__';
 let apolloClient: ApolloClient<NormalizedCacheObject> | undefined;
 
 const createApolloClient = () => {
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
     link: new HttpLink({
-      uri: 'https://hasura-learning13.hasura.app/v1/graphql', // Server URL (must be absolute)
+      uri: process.env.NEXT_PUBLIC_HASURA_URL,
+      headers: {
+        'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_KEY,
+      },
     }),
 
     cache: new InMemoryCache(),
